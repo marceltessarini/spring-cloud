3180aa86e2ecbd51c88f9b51701eaf4ba137396a
Marcel Tessarini <marceltessarini@gmail.com>
2023-05-13 09:54:35 -0300

    Listando ordens

--- order-service/src/main/java/br/com/marceltessarini/controller/OrderController.java
+++ order-service/src/main/java/br/com/marceltessarini/controller/OrderController.java
@@ -1,6 +1,8 @@
 package br.com.marceltessarini.controller;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.marceltessarini.data.OrderData;
+import br.com.marceltessarini.data.OrderView;
 import br.com.marceltessarini.model.Order;
 import br.com.marceltessarini.model.OrderStatus;
 import br.com.marceltessarini.repository.OrderRepository;
@@ -20,6 +23,15 @@ public class OrderController {
 
 	@Autowired
 	private OrderRepository orderRepository;
+	
+    @RequestMapping(value = "/order", method = RequestMethod.GET)
+	public List<OrderView> list() {
+    	List<Order> orders = orderRepository.findAll();
+    	
+    	List<OrderView> ordersView = orders.stream().map(o -> OrderView.fromOrder(o)).collect(Collectors.toList());
+    	
+    	return ordersView;
+    }
 	
 	@RequestMapping(value = "/order", method = RequestMethod.POST)
 	public ResponseEntity<Void> save(@RequestBody OrderData orderData) {
